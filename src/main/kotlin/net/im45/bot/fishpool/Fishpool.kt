557b0f1043f53b261395da3e3f7f7074b0c8e042
45gfg9 <mc45@qq.com>
2020-11-17 23:10:36 +0800

    Overload & Timeout

--- src/main/kotlin/net/im45/bot/fishpool/Fishpool.kt
+++ src/main/kotlin/net/im45/bot/fishpool/Fishpool.kt
@@ -1,10 +1,16 @@
 package net.im45.bot.fishpool
 
 import com.google.auto.service.AutoService
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.*
+import io.ktor.client.request.*
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.unregisterAllCommands
 import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.console.command.UserCommandSender
+import net.mamoe.mirai.console.data.AutoSavePluginConfig
+import net.mamoe.mirai.console.data.value
 import net.mamoe.mirai.console.plugin.jvm.JvmPlugin
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
@@ -32,7 +38,7 @@ object Fishpool : KotlinPlugin(
     override fun onDisable() {
         super.onDisable()
 
-        unregisterAllCommands()
+        unregisterAllCommands(this)
     }
 }
 
@@ -44,6 +50,9 @@ object OhCmd : SimpleCommand(
         val hs = "H".repeat(abs(h.toInt()))
         sendMessage(if (h > 0) "O${hs}" else "${hs}O")
     }
+
+    @Handler
+    suspend fun UserCommandSender.oh() = oh(16)
 }
 
 object NaCmd : SimpleCommand(
@@ -54,6 +63,9 @@ object NaCmd : SimpleCommand(
         val ns = "呐".repeat(abs(n.toInt()))
         if (ns.isNotEmpty()) sendMessage(ns)
     }
+
+    @Handler
+    suspend fun UserCommandSender.na() = na(2)
 }
 
 object PaCmd : SimpleCommand(
@@ -64,34 +76,54 @@ object PaCmd : SimpleCommand(
         val ps = "爬".repeat(abs(p.toInt()))
         if (ps.isNotEmpty()) sendMessage(ps)
     }
+
+    @Handler
+    suspend fun UserCommandSender.pa() = pa(1)
 }
 
 object Errcode : SimpleCommand(
         Fishpool, "errcode",
         description = "err.code"
 ) {
+    private val errcode = URL("https://dev.zapic.moe/err.code")
+    private val client = HttpClient(CIO) {
+        install(HttpTimeout) {
+            connectTimeoutMillis = ErrcodeConfig.connectTimeout
+            requestTimeoutMillis = ErrcodeConfig.requestTimeout
+            socketTimeoutMillis = ErrcodeConfig.socketTimeout
+        }
+    }
+
     private class LJYYSException(host: String) : Exception(host)
 
-    private val ERRCODE = URL("https://dev.zapic.cc/err.code")
+    private object ErrcodeConfig : AutoSavePluginConfig("errcode") {
+        val connectTimeout by value(10000L)
+        val requestTimeout by value(30000L)
+        val socketTimeout by value(10000L)
+    }
+
+    private fun stackTraceFormatter(ex: Throwable, limit: Int = 3) = buildString {
+        append("Exception in thread \"${Thread.currentThread().name}\": $ex")
+        ex.stackTrace.let {
+            it.take(limit).forEach { trace -> append("\n\tat $trace") }
+            if (it.size > limit) append("\n\t... ${it.size - limit} more")
+        }
+    }
 
     @Handler
     suspend fun UserCommandSender.errcode() {
-        ERRCODE.readText().let { ec ->
+        client.runCatching {
+            get<String>(errcode)
+        }.onFailure {
+            sendMessage(stackTraceFormatter(it))
+        }.onSuccess { ec ->
             Socket().runCatching {
-                connect(InetSocketAddress(ec, 25565), 500)
-                close()
+                use { connect(InetSocketAddress(ec, 25565), 500) }
             }.let { res ->
-                val out = LJYYSException(if (res.isFailure) "Closed" else ec).run {
-                    val lim = 3
-                    val sb = StringBuilder("Exception in thread \"${Thread.currentThread().name}\": $this")
-                    stackTrace.take(lim).forEach { sb.append("\n\tat $it") }
-                    if (stackTrace.size > lim) sb.append("\n\t... ${stackTrace.size - lim} more")
-
-                    sb.toString()
-                }
-
+                val out = stackTraceFormatter(LJYYSException(if (res.isFailure) "Closed" else ec))
                 sendMessage(out)
             }
         }
     }
 }
+
